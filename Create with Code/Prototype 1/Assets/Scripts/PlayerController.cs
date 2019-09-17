a80162c639cbbd6548ba1dfc8ff69b7850c5673d
Koa Graham <koa.g921@stu.nebo.edu>
2019-09-17 10:28:09 -0600

    Player Input

--- Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
-	public float turnspeed;
-	publicfloat horizontalInput;
+    // Private Variables
+    private float speed = 5.0f;
+	private float turnspeed = 25.0f;
+	private float horizontalInput;
+	private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +19,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		horizontalInput = Input.GetAxis("horizontal");
+        // Player Input
+		horizontalInput = Input.GetAxis("Horizontal");
+		forwardInput = Input.GetAxis("Vertical");
 
         // we'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-		transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horizontalInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+		transform.Rotate(Vector3.up, Time.deltaTime * turnspeed * horizontalInput);
 	}
 } 
