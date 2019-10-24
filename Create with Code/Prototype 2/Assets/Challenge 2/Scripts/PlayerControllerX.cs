d42a79d5b316906e2c2761f5f17852280c902881
Koa Graham <koa.g921@stu.nebo.edu>
2019-10-23 21:29:32 -0600

    Challenge 2: Bonus 2 Complete

--- Create with Code/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Create with Code/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-
+    public float firerate = 1.0f;
+    private float nextFire = 0f;
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
         {
+            nextFire = Time.time + firerate;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
