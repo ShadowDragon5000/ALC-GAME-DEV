5e68765bc6bbeab0bb48828beafa1fec1aef50ab
Koa Graham <koa.g921@stu.nebo.edu>
2019-10-29 09:43:25 -0600

    Challenge 2 finished w/ all bonuses

--- Create with Code/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Create with Code/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,8 +6,7 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
     public float fireDelta = 1.0f;
-    private float nextFire = 0.5f;
-    private float myTime = 0.0f;
+    private float nextFire = 0.0f;
 
     // Update is called once per frame
     void Update()
@@ -17,9 +16,6 @@ public class PlayerControllerX : MonoBehaviour
         {
             nextFire = Time.time + fireDelta;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
-
-            nextFire = nextFire - myTime;
-            myTime = 0.0f;
         }
     }
 }
